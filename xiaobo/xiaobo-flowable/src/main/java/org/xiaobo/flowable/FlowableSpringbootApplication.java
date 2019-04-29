e78b50809175b4d8c704c3af230f39b841829de0
xiaobo <603786093@qq.com>
2019-04-29 10:38:22 +0800

    flowable

---
+++ xiaobo/xiaobo-flowable/src/main/java/org/xiaobo/flowable/FlowableSpringbootApplication.java
@@ -0,0 +1,20 @@
+package org.xiaobo.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * FlowableSpringbootApplication
+ *
+ * @author puhaiyang
+ * @date 2018/12/19
+ */
+@SpringBootApplication
+public class FlowableSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableSpringbootApplication.class, args);
+    }
+
+}
+
