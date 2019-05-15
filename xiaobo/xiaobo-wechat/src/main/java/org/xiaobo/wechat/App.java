9da8ad4e478eaafa63d469cc3249f9702fcbb4f0
xiaobo <603786093@qq.com>
2019-05-15 20:19:41 +0800

    11111

---
+++ xiaobo/xiaobo-wechat/src/main/java/org/xiaobo/wechat/App.java
@@ -0,0 +1,15 @@
+package org.xiaobo.wechat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String mobile="15600185699";
+        System.out.println(mobile.substring(mobile.length()-6));
+    }
+}
