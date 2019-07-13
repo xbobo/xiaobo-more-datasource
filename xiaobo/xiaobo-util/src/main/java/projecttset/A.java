224e2aee93468dba0b0d3986b9c7d2d6b494d86b
xiaobo <603786093@qq.com>
2019-07-13 14:06:57 +0800

    测试提交

---
+++ xiaobo/xiaobo-util/src/main/java/projecttset/A.java
@@ -0,0 +1,27 @@
+package projecttset;
+
+public class A {
+
+	public static void main(String[] args) {
+//		List<UserVO> list=new  ArrayList<UserVO>();
+//		Set<UserVO> set=new HashSet<UserVO>();
+//		for(int i=0;i<10;i++) {
+//			UserVO vo=new UserVO();
+//			vo.setUserId(1L);
+//			vo.setUserNickname("name"+i);
+//			if(list.contains(vo)) {
+//				System.out.println("contains");
+//			}else {
+//				list.add(vo);
+//			}
+//			set.add(vo);
+//		}
+//        System.out.println(list.size());
+//         
+//        System.out.println(set.size());
+		
+		String name="https://topic.puxinwangxiao.com/joint?aId=2019070110001002&recommend_id=61455615982346240";
+		System.out.println(name.length());
+	}
+
+}
