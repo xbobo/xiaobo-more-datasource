2af2f8b2e6051294c7baf204729ca61726bf8801
xiaobo <603786093@qq.com>
2019-08-06 16:01:24 +0800

    11

---
+++ xiaobo/xiaobo-util/src/main/java/date/DateTest.java
@@ -0,0 +1,18 @@
+package date;
+
+import weixin.nati.DateUtil;
+
+/**
+ * @author xiaobo
+ * @date 2019/8/1
+ * @description:
+ */
+public class DateTest {
+
+    public static void main(String[] args) {
+//        System.out.println(DateUtil.getDayStr(1564668000L, 1564675200L));
+//        System.out.println(DateUtil.getDayStr(1564754400L, 1564675200L));
+//        System.out.println(DateUtil.getDayStr(1564675200L, 1564754400L));
+        System.out.println(new String("\u548c\u5e73\u533a"));
+    }
+}
